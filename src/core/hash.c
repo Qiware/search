cb7f145747b8a84cd8b24a9c458e4e97cf438d3e
royalapex <qifeng.zou@gmail.com>
2014-09-04 12:00:29 +0800

    [HASH]: 添加TIME33哈希算法

--- src/core/hash.c
+++ src/core/hash.c
@@ -15,6 +15,7 @@
  ** Return: 哈希值
  ** Proc :
  ** Note :
+ **     该算法特点: 算法简单、性能高效、散列效果佳.
  ** Author: # Qifeng.zou # 2014.04.18 #
  ******************************************************************************/
 uint32_t hash_time33(const char *str)
@@ -23,6 +24,32 @@ uint32_t hash_time33(const char *str)
     uint32_t hash = 5381;
 
     while (*p)
+    {
+        hash += (hash << 5) + (*p++);
+    }
+
+    return (hash & 0x7FFFFFFF);
+}
+
+/******************************************************************************
+ ** Name : hash_time33_ex
+ ** Desc : TIME33哈希算法
+ ** Input: 
+ **     addr: 内存地址
+ **     len: 长度
+ ** Output: NONE
+ ** Return: 哈希值
+ ** Proc :
+ ** Note :
+ **     该算法特点: 算法简单、性能高效、散列效果佳.
+ ** Author: # Qifeng.zou # 2014.04.18 #
+ ******************************************************************************/
+uint32_t hash_time33_ex(const char *addr, int len)
+{
+    uint32_t hash = 5381;
+    const char *p = addr;
+
+    while (len-- > 0)
     {
         hash += (hash << 5) + (*p++);
     }
--- src/incl/hash.h
+++ src/incl/hash.h
@@ -4,5 +4,6 @@
 #include <stdint.h>
 
 uint32_t hash_time33(const char *str);
+uint32_t hash_time33_ex(const char *addr, int len);
 
 #endif /*__HASH_H__*/
