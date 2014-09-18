f9b9d4c0aa64fd7a09d1c4ada9660974f220e219
Qifeng <Qifeng.zou.job@gmail.com>
2014-09-18 23:01:10 +0800

    [STRING]: 添加字串转换函数

--- src/core/x_string.c
+++ src/core/x_string.c
@@ -3,7 +3,7 @@
 #include "x_string.h"
 
 /******************************************************************************
- **函数名称: str_to_low
+ **函数名称: str_to_lower
  **功    能: 将字串转为小字母
  **输入参数:
  **     str: 字串
@@ -13,7 +13,7 @@
  **注意事项:
  **作    者: # Qifeng.zou # 2014.09.18 #
  ******************************************************************************/
-string_t *str_to_low(string_t *s)
+string_t *str_to_lower(string_t *s)
 {
     int idx;
 
@@ -49,6 +49,69 @@ string_t *str_to_low(string_t *s)
             case 'Z':
             {
                 s->str[idx] += 32;
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
+    }
+
+    return s;
+}
+
+/******************************************************************************
+ **函数名称: str_to_upper
+ **功    能: 将字串转为大字母
+ **输入参数:
+ **     str: 字串
+ **输出参数:
+ **返    回: 字串对象
+ **实现描述: 
+ **注意事项:
+ **作    者: # Qifeng.zou # 2014.09.18 #
+ ******************************************************************************/
+string_t *str_to_upper(string_t *s)
+{
+    int idx;
+
+    for (idx=0; idx<s->len; ++idx)
+    {
+        switch (s->str[idx])
+        {
+            case 'a':
+            case 'b':
+            case 'c':
+            case 'd':
+            case 'e':
+            case 'f':
+            case 'g':
+            case 'h':
+            case 'i':
+            case 'j':
+            case 'k':
+            case 'l':
+            case 'm':
+            case 'n':
+            case 'o':
+            case 'p':
+            case 'q':
+            case 'r':
+            case 's':
+            case 't':
+            case 'u':
+            case 'v':
+            case 'w':
+            case 'x':
+            case 'y':
+            case 'z':
+            {
+                s->str[idx] -= 32;
+                break;
+            }
+            default:
+            {
                 break;
             }
         }
--- src/incl/x_string.h
+++ src/incl/x_string.h
@@ -8,6 +8,7 @@ typedef struct
     int len;        /* 字串长 */
 } string_t;
  
-string_t *str_to_low(string_t *s);
+string_t *str_to_lower(string_t *s);
+string_t *str_to_upper(string_t *s);
 
 #endif /*__X_STRING_H__*/
