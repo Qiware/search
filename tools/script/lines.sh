c39507303a04fc8ef702cbb577757821805d2dd9
Qifeng.zou <Qifeng.zou.job@gmail.com>
2014-12-28 13:24:14 +0800

    [Script]: 完善代码行数统计功能

--- tools/script/lines.sh
+++ tools/script/lines.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
-hl=`find ../../ -name "*.h" | xargs grep "^$" | wc -l`
-cl=`find ../../ -name "*.c" | xargs grep "^$" | wc -l`
-total=`expr $hl + $cl`
-echo empty lines: $hl + $cl = $total
-
-hl=`find ../../ -name "*.h" | xargs grep -v "^$" | wc -l`
-cl=`find ../../ -name "*.c" | xargs grep -v "^$" | wc -l`
-total=`expr $hl + $cl`
-echo non-empty lines: $hl + $cl = $total
-
+# 总行数
 hl=`find ../../ -name "*.h" | xargs cat | wc -l`
 cl=`find ../../ -name "*.c" | xargs cat | wc -l`
 total=`expr $hl + $cl`
-echo total lines: $hl + $cl = $total
+
+# 空行数
+hl=`find ../../ -name "*.h" | xargs grep "^$" | wc -l`
+cl=`find ../../ -name "*.c" | xargs grep "^$" | wc -l`
+empty=`expr $hl + $cl`
+
+# 注释数
+hl=`find ../../ -name "*.h" | xargs grep "\*\*" | wc -l`
+cl=`find ../../ -name "*.c" | xargs grep "\*\*" | wc -l`
+note=`expr $hl + $cl`
+
+# 实行数
+hl=`find ../../ -name "*.h" | xargs grep -v "^$" | grep -v "\*\*" | wc -l`
+cl=`find ../../ -name "*.c" | xargs grep -v "^$" | grep -v "\*\*" | wc -l`
+real=`expr $hl + $cl`
+
+echo "总行数  空行数  注释数  实行数"
+echo "$total\t$empty\t$note\t$real"
