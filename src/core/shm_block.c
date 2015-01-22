daf7593ec8b4bbd916eda47ca70728d212a5ccca
Qifeng.zou <Qifeng.zou.job@gmail.com>
2015-01-23 00:49:21 +0800

    [SHM-BLOCK]: 完善共享内存内存块池算法

--- src/core/shm_block.c
+++ src/core/shm_block.c
@@ -14,35 +14,53 @@
 #include "shm_opt.h"
 #include "shm_block.h"
 
+#define shm_block_get_page_addr(blk, n) /* 获取页地址 */\
+    (shm_block_page_t *)((void *)(blk) + (blk)->page_off + (n) * sizeof(shm_block_page_t))
+
 
 /******************************************************************************
  **函数名称: shm_block_creat
  **功    能: 创建内存池
  **输入参数: 
  **     num: 内存块数
- **     size: 内存块大小
+ **     unit_size: 内存块大小
  **输出参数: NONE
  **返    回: 内存池对象
  **实现描述: 
  **注意事项: 
+ ** 内存池算法的空间布局:
+ **     ------ -------- -----------------------------------------------
+ **    |\\\\\\|XX|XX|XX|////////|////////|/////////|/////////|/////////|
+ **    |\对象\|X页对象X|///////可///////用////////空////////间/////////|
+ **    |\\\\\\|XX|XX|XX|////////|////////|/////////|/////////|/////////|
+ **     ------ -------- -----------------------------------------------
+ **    ^      ^        ^                                               ^
+ **    |      |        |                                               |
+ **   addr   page     data                                            end
  **作    者: # Qifeng.zou # 2015.01.22 #
  ******************************************************************************/
-shm_block_t *shm_block_creat(int num, size_t size)
+shm_block_t *shm_block_creat(const char *path, int num, size_t unit_size)
 {
-    key_t key = 0x033034;
+    key_t key;
     void *addr;
-    int i, m, idx, pages;
+    int i, m, idx, page_num, n;
     uint32_t *bitmap;
     size_t shm_size;
     shm_block_t *blk;
     shm_block_page_t *page;
 
     /* 1. 计算内存大小 */
-    pages = div_ceiling(num, SHM_BLOCK_PAGE_SLOT_NUM);
+    page_num = div_ceiling(num, SHM_BLOCK_PAGE_SLOT_NUM);
 
-    shm_size = sizeof(shm_block_t) + pages * sizeof(shm_block_page_t) + num * size;
+    shm_size = sizeof(shm_block_t) + page_num * sizeof(shm_block_page_t) + num * unit_size;
 
     /* 2. 创建共享内存 */
+    key = shm_ftok(path, 0);
+    if (-1 == key)
+    {
+        return NULL;
+    }
+
     addr = shm_creat(key, shm_size);
     if (NULL == addr)
     {
@@ -53,44 +71,55 @@ shm_block_t *shm_block_creat(int num, size_t size)
     blk = (shm_block_t *)addr;
 
     blk->num = num;
-    blk->size = size;
-    blk->pages = pages;
-    blk->page = (shm_block_page_t *)(addr + sizeof(shm_block_t));
-    blk->addr = (char *)(addr + sizeof(shm_block_t) + pages * sizeof(shm_block_page_t));
+    blk->unit_size = unit_size;
+    blk->page_num = page_num;
+    blk->page_off = sizeof(shm_block_t);
+    blk->data_off = blk->page_off + page_num * sizeof(shm_block_page_t);
 
     /* 4. 设置位图信息 */
-    for (idx=0; idx<blk->pages; ++idx)
+    page = (shm_block_page_t *)(addr + blk->page_off);
+
+    for (idx=0; idx<blk->page_num; ++idx, ++page)
     {
-        page = &blk->page[idx];
 
         spin_lock_init(&page->lock);
 
         /* 3.1 设置bitmap */
-        if (idx == (blk->pages - 1))
+        if (idx == (blk->page_num - 1))
         {
             m = (num - idx*SHM_BLOCK_PAGE_SLOT_NUM) % 32;
 
-            page->bitmaps = div_ceiling(num - idx*SHM_BLOCK_PAGE_SLOT_NUM, 32);
+            page->bitmap_num = div_ceiling(num - idx*SHM_BLOCK_PAGE_SLOT_NUM, 32);
         }
         else
         {
             m = SHM_BLOCK_PAGE_SLOT_NUM % 32;
 
-            page->bitmaps = div_ceiling(SHM_BLOCK_PAGE_SLOT_NUM, 32);
+            page->bitmap_num = div_ceiling(SHM_BLOCK_PAGE_SLOT_NUM, 32);
         }
 
         if (m)
         {
-            bitmap = &page->bitmap[page->bitmaps - 1];
+            bitmap = &page->bitmap[page->bitmap_num - 1];
 
             for (i=m; i<32; i++)
             {
                 *bitmap |= (1 << i);
             }
+
+            n = page->bitmap_num;
+            bitmap = &page->bitmap[page->bitmap_num];
+            for (; n < SHM_BLOCK_PAGE_SLOT_NUM/32; ++n, ++bitmap)
+            {
+                for (i=0; i<32; i++)
+                {
+                    *bitmap |= (1 << i);
+                }
+            }
         }
 
         /* 3.2 设置数据空间 */
-        page->addr = blk->addr + idx * SHM_BLOCK_PAGE_SLOT_NUM * size;
+        page->data_off = blk->data_off + idx * SHM_BLOCK_PAGE_SLOT_NUM * unit_size;
     }
 
     return blk;
@@ -109,20 +138,21 @@ shm_block_t *shm_block_creat(int num, size_t size)
  ******************************************************************************/
 void *shm_block_alloc(shm_block_t *blk)
 {
+    void *addr = (void *)blk;
     uint32_t i, j, n, p;
     uint32_t *bitmap;
     shm_block_page_t *page;
 
     n = rand(); /* 随机选择页 */
 
-    for (p=0; p<blk->pages; ++p, ++n)
+    for (p=0; p<blk->page_num; ++p, ++n)
     {
-        n %= blk->pages;
-        page = &blk->page[n];
+        n %= blk->page_num;
+        page = shm_block_get_page_addr(blk, n);
 
         spin_lock(&page->lock);
 
-        for (i=0; i<page->bitmaps; ++i)
+        for (i=0; i<page->bitmap_num; ++i)
         {
             if (0xFFFFFFFF == page->bitmap[i])
             {
@@ -138,7 +168,7 @@ void *shm_block_alloc(shm_block_t *blk)
 
                     spin_unlock(&page->lock);
 
-                    return page->addr + (i*32 + j)*blk->size;
+                    return (addr + page->data_off + (i*32 + j)*blk->unit_size);
                 }
             }
         }
@@ -163,14 +193,19 @@ void *shm_block_alloc(shm_block_t *blk)
 void shm_block_dealloc(shm_block_t *blk, void *p)
 {
     int i, j, n;
+    shm_block_page_t *page;
+    void *addr = (void *)blk;
 
-    n = (p - blk->addr) / (SHM_BLOCK_PAGE_SLOT_NUM * blk->size);        /* 计算页号 */
-    i = (p - blk->page[n].addr) / (32 * blk->size);                     /* 计算页内bitmap索引 */
-    j = (p - (blk->page[n].addr + i * (32 * blk->size))) / blk->size;   /* 计算bitmap内偏移 */
+    n = (p - addr) / (SHM_BLOCK_PAGE_SLOT_NUM * blk->unit_size);   /* 计算页号 */
 
-    spin_lock(&blk->page[n].lock);
-    blk->page[n].bitmap[i] &= ~(1 << j);
-    spin_unlock(&blk->page[n].lock);
+    page = shm_block_get_page_addr(blk, n);
+
+    i = (p - (addr + page->data_off)) / (32 * blk->unit_size);     /* 计算页内bitmap索引 */
+    j = (p - ((addr + page->data_off) + i * (32 * blk->unit_size))) / blk->unit_size;   /* 计算bitmap内偏移 */
+
+    spin_lock(&page->lock);
+    page->bitmap[i] &= ~(1 << j);
+    spin_unlock(&page->lock);
 }
 
 /******************************************************************************
--- src/incl/shm_block.h
+++ src/incl/shm_block.h
@@ -1,3 +1,14 @@
+/******************************************************************************
+ ** 内存池算法的空间布局:
+ **     ------ -------- -----------------------------------------------
+ **    |\\\\\\|XX|XX|XX|////////|////////|/////////|/////////|/////////|
+ **    |\对象\|X页对象X|///////可///////用////////空////////间/////////|
+ **    |\\\\\\|XX|XX|XX|////////|////////|/////////|/////////|/////////|
+ **     ------ -------- -----------------------------------------------
+ **    ^      ^        ^                                               ^
+ **    |      |        |                                               |
+ **   addr   page     data                                            end
+ *******************************************************************************/
 #if !defined(__SHM_BLOCK_H__)
 #define __SHM_BLOCK_H__
 
@@ -10,25 +21,23 @@
 typedef struct
 {
     spinlock_t lock;                /* SPIN锁对象 */
-
-    int bitmaps;                    /* 位图长度 */
+    uint32_t bitmap_num;            /* 实际使用的BITMAP数 */
     uint32_t bitmap[SHM_BLOCK_PAGE_SLOT_NUM/32];  /* 分配位图(0:未使用 1:使用) */
-    void *addr;                     /* 各页内存起始地址(指向shm_block_t->addr中的内存空间) */
+    size_t data_off;               /* 相对于data的偏移 */
 } shm_block_page_t;
 
-/* 内存数据块对象 */
+/* 数据块对象 */
 typedef struct
 {
     int num;                        /* 内存块总数 */
-    int pages;                      /* 总页数 */
-    size_t size;                    /* 各内存块SIZE */
+    int page_num;                   /* 总页数 */
+    size_t unit_size;               /* 各内存块SIZE */
 
-    void *addr;                     /* 总内存起始地址 */
-
-    shm_block_page_t *page;         /* 内存页 */
+    size_t page_off;                /* 页对象 */
+    size_t data_off;                /* 可支配空间 */
 } shm_block_t;
 
-shm_block_t *shm_block_creat(int num, size_t size);
+shm_block_t *shm_block_creat(const char *path, int num, size_t unit_size);
 void *shm_block_alloc(shm_block_t *blk);
 void shm_block_dealloc(shm_block_t *blk, void *p);
 void shm_block_destroy(shm_block_t *blk);
