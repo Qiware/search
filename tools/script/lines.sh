4100890c13041252503e465a5d70d40e9ef32d52
Qifeng.zou <Qifeng.zou.job@gmail.com>
2015-02-07 04:05:51 +0800

    [Script]: 完善代码行统计脚本

--- tools/script/lines.sh
+++ tools/script/lines.sh
@@ -11,14 +11,12 @@ cl=`find ../../ -name "*.c" | xargs grep "^$" | wc -l`
 empty=`expr $hl + $cl`
 
 # 注释数
-hl=`find ../../ -name "*.h" | xargs grep "\*\*" | wc -l`
-cl=`find ../../ -name "*.c" | xargs grep "\*\*" | wc -l`
+hl=`find ../../ -name "*.h" | xargs grep -e "\*\*" -e "/\*" -e "\*\/" | wc -l`
+hl=`find ../../ -name "*.c" | xargs grep -e "\*\*" -e "/\*" -e "\*\/" | wc -l`
 note=`expr $hl + $cl`
 
 # 实行数
-hl=`find ../../ -name "*.h" | xargs grep -v "^$" | grep -v "\*\*" | wc -l`
-cl=`find ../../ -name "*.c" | xargs grep -v "^$" | grep -v "\*\*" | wc -l`
-real=`expr $hl + $cl`
+real=`expr $total - $empty - $note`
 
 echo "总行数  空行数  注释数  实行数"
 echo "$total\t$empty\t$note\t$real"
