bd13b1a573ee2e2bb82e02c5d8ff8c96acb53e0d
Qifeng.zou <Qifeng.zou.job@gmail.com>
2015-03-03 11:33:41 +0800

    [HTTP]: 添加HTTP应答信息的提取接口

--- src/comm/http/http.c
+++ src/comm/http/http.c
@@ -21,7 +21,7 @@
  **返    回: 0:成功 !0:失败
  **实现描述: 
  **注意事项: 
- **作    者: # Menglai.Wang # 2014.09.23 #
+ **作    者: # Qifeng.zou # 2014.09.23 #
  ******************************************************************************/
 int http_get_request(const char *uri, char *req, int size)
 {
@@ -43,6 +43,134 @@ int http_get_request(const char *uri, char *req, int size)
         "Connection: close\r\n"
         "User-Agent: XunDao Crawler\r\n"
         "\r\n", field.path, field.host);
+
+    return HTTP_OK;
+}
+
+/******************************************************************************
+ **函数名称: http_parse_response
+ **功    能: 解析HTTP应答
+ **输入参数:
+ **     str: 应答字串
+ **输出参数:
+ **     resp: 应答信息
+ **返    回: 0:成功 !0:失败
+ **实现描述: 
+ **注意事项: 
+ **作    者: # Qifeng.zou # 2015.03.03 #
+ ******************************************************************************/
+int http_parse_response(const char *str, http_response_t *rep)
+{
+    int len;
+    const char *p, *end, *tmp;
+
+    end = strstr(str, "\r\n\r\n");
+    if (NULL == end)
+    {
+        return HTTP_ERR; /* 不完整 */
+    }
+
+    p = str;
+    while (' ' == *p) { ++p; }
+
+    /* > 获取版本, 状态 */
+    if (!strncasecmp(p, HTTP_KEY_VERS_09, strlen(HTTP_KEY_VERS_09)))
+    {
+        rep->version = HTTP_VERSION_09;
+        len = strlen(HTTP_KEY_VERS_09);
+    }
+    else if (!strncasecmp(p, HTTP_KEY_VERS_10, strlen(HTTP_KEY_VERS_10)))
+    {
+        rep->version = HTTP_VERSION_10;
+        len = strlen(HTTP_KEY_VERS_10);
+    }
+    else if (!strncasecmp(p, HTTP_KEY_VERS_11, strlen(HTTP_KEY_VERS_11)))
+    {
+        rep->version = HTTP_VERSION_11;
+        len = strlen(HTTP_KEY_VERS_11);
+    }
+    else if (!strncasecmp(p, HTTP_KEY_VERS_20, strlen(HTTP_KEY_VERS_20)))
+    {
+        rep->version = HTTP_VERSION_20;
+        len = strlen(HTTP_KEY_VERS_20);
+    }
+    else
+    {
+        return HTTP_ERR;
+    }
+
+    p += len;
+    tmp = p;
+    while (isdigit(*tmp))
+    {
+        rep->status *= 10;
+        rep->status += (*tmp - '0');
+        ++tmp;
+    }
+
+    if (rep->status < 100 || rep->status > 999)
+    {
+        return HTTP_ERR;
+    }
+    else if (200 != rep->status)
+    {
+        return HTTP_OK;
+    }
+
+    p = strstr(p, "\r\n");
+    if (p == end)
+    {
+        return HTTP_ERR;
+    }
+
+    p += 2;
+
+    /* > 获取其他信息 */
+    while (p != end)
+    {
+        while (' ' == *p) { ++p; }
+        if (p == end)
+        {
+            break;
+        }
+
+        /* 连接方式 */
+        if (!strncasecmp(p, HTTP_KEY_CONNECTION, strlen(HTTP_KEY_CONNECTION)))
+        {
+            p += strlen(HTTP_KEY_CONNECTION);
+            while (' ' == *p) { ++p; }
+            if (!strncasecmp(p, HTTP_KEY_CONNECTION_CLOSE, strlen(HTTP_KEY_CONNECTION_CLOSE)))
+            {
+                rep->connection = HTTP_CONNECTION_CLOSE;
+            }
+            else if (!strncasecmp(p, HTTP_KEY_CONNECTION_KEEPALIVE, strlen(HTTP_KEY_CONNECTION_KEEPALIVE)))
+            {
+                rep->connection = HTTP_CONNECTION_KEEPALIVE;
+            }
+            else
+            {
+                return HTTP_ERR;
+            }
+        }
+        /* 内容长度 */
+        else if (!strncasecmp(p, HTTP_KEY_CONTENT_LEN, strlen(HTTP_KEY_CONTENT_LEN)))
+        {
+            p += strlen(HTTP_KEY_CONTENT_LEN);
+            while (' ' == *p) { ++p; }
+            while (isdigit(*p))
+            {
+                rep->content_len *= 10;
+                rep->content_len += (*p - '0');
+            }
+        }
+
+        p = strstr(p, "\r\n");
+        if (p == end)
+        {
+            break;
+        }
+        p += 2;
+    }
 
     return HTTP_OK;
 }
--- src/incl/http.h
+++ src/incl/http.h
@@ -3,15 +3,47 @@
 
 #include "common.h"
 
+/* 关键词定义 */
+#define HTTP_KEY_VERS_09        "HTTP/0.9 "
+#define HTTP_KEY_VERS_10        "HTTP/1.0 "
+#define HTTP_KEY_VERS_11        "HTTP/1.1 "
+#define HTTP_KEY_VERS_20        "HTTP/2.0 "
+
+#define HTTP_KEY_CONNECTION     "Connection:"
+#define HTTP_KEY_CONTENT_LEN    "Content-Length:"
+
+#define HTTP_KEY_CONNECTION_CLOSE       "Close"
+#define HTTP_KEY_CONNECTION_KEEPALIVE   "Keep-alive"
+
 #define HTTP_DEF_PORT           (80)    /* HTTP默认端口号 */
 #define HTTP_GET_REQ_STR_LEN    (3072)  /* HTTP GET请求的最大长度 */
 
 typedef enum
 {
-    HTTP_OK                     /* 正常 */
+    HTTP_OK                             /* 正常 */
 
-    , HTTP_ERR                  /* 异常 */
+    , HTTP_ERR                          /* 异常 */
 } http_err_code_e;
+
+typedef enum
+{
+    HTTP_VERSION_09                     /* HTTP/0.9 */
+    , HTTP_VERSION_10                   /* HTTP/1.0 */
+    , HTTP_VERSION_11                   /* HTTP/1.1 */
+    , HTTP_VERSION_20                   /* HTTP/2.0 */
+
+    , HTTP_VERSION_TOTAL
+} http_version_e;
+
+typedef struct
+{
+    int version;                        /* 版本号 */
+    int status;                         /* 状态码 */
+    int content_len;                    /* 应答内容长度 */
+#define HTTP_CONNECTION_CLOSE       (0) /* 关闭 */
+#define HTTP_CONNECTION_KEEPALIVE   (1) /* 保活 */
+    int connection;                     /* 连接方式 */
+} http_response_t;
 
 int http_get_request(const char *uri, char *req, int size);
 
