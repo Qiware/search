8041c21a4eef92944c5a765850f5577f9bc5eaa6
Qifeng.zou <Qifeng.zou.job@gmail.com>
2015-03-16 12:29:30 +0800

    [Monitor]: 删除头文件

--- src/exec/monitor/monitor.h
+++
@@ -1,9 +0,0 @@
-#if !defined(__MONITOR_H__)
-#define __MONITOR_H__
-
-#include "menu.h"
-
-menu_item_t *mon_srch_menu(menu_cntx_t *ctx);
-menu_item_t *mon_crwl_menu(menu_cntx_t *ctx);
-
-#endif /*__MONITOR_H__*/
