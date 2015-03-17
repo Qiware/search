5f3db3ef50a05e9f4731718ca5f2279182d13a0c
Qifeng.zou <Qifeng.zou.job@gmail.com>
2015-03-17 22:26:50 +0800

    [Script]: 提高代码的统计精度 - 不统计测试代码

--- tools/script/lines.sh
+++ tools/script/lines.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 
+DIR="../../src"
+SRC="$DIR/comm $DIR/core $DIR/exec $DIR/incl"
+
 # 总行数
-hl=`find ../../ -name "*.h" | xargs cat | wc -l`
-cl=`find ../../ -name "*.c" | xargs cat | wc -l`
+hl=`find $SRC -name "*.h" | xargs cat | wc -l`
+cl=`find $SRC -name "*.c" | xargs cat | wc -l`
 total=`expr $hl + $cl`
 
 # 空行数
-hl=`find ../../ -name "*.h" | xargs grep "^$" | wc -l`
-cl=`find ../../ -name "*.c" | xargs grep "^$" | wc -l`
+hl=`find $SRC -name "*.h" | xargs grep "^$" | wc -l`
+cl=`find $SRC -name "*.c" | xargs grep "^$" | wc -l`
 empty=`expr $hl + $cl`
 
 # 注释数
-hl=`find ../../ -name "*.h" | xargs grep \
+hl=`find $SRC -name "*.h" | xargs grep \
    -e "^[[:space:]]\{0,\}/\*" \
    -e "^[[:space:]]\{0,\}\*" \
    -e "^[[:space:]]\{0,\}\*\*" \
    -e "^[[:space:]]\{0,\}\*/" \
    -e "^[[:space:]]\{0,\}//" | wc -l`
-cl=`find ../../ -name "*.c" | xargs grep \
+cl=`find $SRC -name "*.c" | xargs grep \
    -e "^[[:space:]]\{0,\}/\*" \
    -e "^[[:space:]]\{0,\}\*" \
    -e "^[[:space:]]\{0,\}\*\*" \
