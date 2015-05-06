26b57fc6853f5e0ed3df21c344d49c474a967114
Qifeng.zou <Qifeng.zou.job@gmail.com>
2015-05-06 12:56:39 +0800

    [SHM-SLOT]: 添加更加高效的共享内存版的内存池机制

---
+++ src/incl/shm_slot.h
@@ -0,0 +1,19 @@
+#if !defined(__SHM_SLOT_H__)
+#define __SHM_SLOT_H__
+
+#include "comm.h"
+
+/* SLOT头 */
+typedef struct
+{
+    int max;                    /* 内存块数 */
+    int size;                   /* 内存块大小 */
+} shm_slot_t;
+
+size_t shm_slot_total(int num, size_t size);
+int shm_slot_init(void *addr, int num, size_t size);
+void *shm_slot_alloc(shm_slot_t *slot);
+void shm_slot_dealloc(shm_slot_t *slot, void *p);
+void shm_slot_destroy(shm_slot_t *slot);
+
+#endif /*__SHM_SLOT_H__*/
