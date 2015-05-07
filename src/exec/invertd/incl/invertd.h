773c73a216a1f49ca0af96762e14caffa225aa5c
Qifeng.zou <Qifeng.zou.job@gmail.com>
2015-05-07 14:10:09 +0800

    [INVERTD]: 完善倒排服务初始化流程

--- src/exec/invertd/incl/invertd.h
+++ src/exec/invertd/incl/invertd.h
@@ -10,12 +10,14 @@
 #define __INVERTD_H__
 
 #include "log.h"
+#include "sdtp.h"
 #include "invert_tab.h"
 
 typedef struct
 {
     log_cycle_t *log;                       /* 日志对象 */
-    invert_tab_t *tab;                      /* 倒排表 */
+    sdtp_cntx_t *sdtp;                      /* SDTP服务 */
+    invt_tab_t *tab;                        /* 倒排表 */
 } invtd_cntx_t;
 
 #endif /*__INVERTD_H__*/
