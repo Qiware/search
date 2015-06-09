fdbdd23fadcfc49ae734c1de040659ce6c9f4894
Qifeng.zou <Qifeng.zou.job@gmail.com>
2015-06-09 15:35:44 +0800

    [SHA-256]: 整理代码

--- src/lib/core/sha256.c
+++ src/lib/core/sha256.c
@@ -12,7 +12,7 @@
 
 static uint64_t sha256_calc_block(int* DwordBufPtr, int* regPtr);
 
-const int sha256_key[ 64 ] = {
+const int sha256_key[64] = {
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
     0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
@@ -57,7 +57,7 @@ uint64_t sha256_reset(sha256_t* t)
 
     for (x = 0; x < 16; x ++)
     {
-        t->Padding[ x ] = t->DwordBuf[ x ] = 0;
+        t->Padding[x] = t->DwordBuf[x] = 0;
     }
 
     t->ByteNumLo = t->ByteNumHi = t->DwordBufBytes = 0;
@@ -68,7 +68,7 @@ uint64_t sha256_reset(sha256_t* t)
 uint64_t sha256_calculate(sha256_t* t, char* dp, uint64_t dl)
 {
     int w,x,y,z;
-    int reg[ 8 ];
+    int reg[8];
 
     /** 
        Set x, y, z, w 
@@ -86,8 +86,8 @@ uint64_t sha256_calculate(sha256_t* t, char* dp, uint64_t dl)
     t->ByteNumLo = z; 
     t->ByteNumHi = y;
 
-    for (w = 0; w < 16; w ++) { t->Padding[ w ] = t->DwordBuf[ w ]; }
-    for (w = 0; w < 8; w ++) { reg[ w ] = t->reg[ w ]; }
+    for (w = 0; w < 16; w ++) { t->Padding[w] = t->DwordBuf[w]; }
+    for (w = 0; w < 8; w ++) { reg[w] = t->reg[w]; }
     x = t->DwordBufBytes / 4;
     y = (t->DwordBufBytes & 3) * 8;
     w = (int)(((t->DwordBufBytes + dl) >> 6) & 0x03ffffff);
@@ -98,8 +98,8 @@ uint64_t sha256_calculate(sha256_t* t, char* dp, uint64_t dl)
     */
     while (w > 0) {
         while (x < 16) {
-            t->Padding[ x ] = (t->Padding[ x ] & ~(0x000000ff << (24 - y)))
-                                    | ((dp[ z ] & 0x000000ff) << (24 - y));
+            t->Padding[x] = (t->Padding[x] & ~(0x000000ff << (24 - y)))
+                                    | ((dp[z] & 0x000000ff) << (24 - y));
             y = y + 8; x += (y >> 5); y &= 0x1f; z ++; 
         }
         sha256_calc_block(t->Padding, reg);
@@ -110,7 +110,7 @@ uint64_t sha256_calculate(sha256_t* t, char* dp, uint64_t dl)
     if ((x | y) == 0) {
         for (w = 0; w < 16; w ++)
         {
-            t->DwordBuf[ w ] = t->Padding[ w ] = 0;
+            t->DwordBuf[w] = t->Padding[w] = 0;
         }
     }
 
@@ -120,38 +120,38 @@ uint64_t sha256_calculate(sha256_t* t, char* dp, uint64_t dl)
     */
     w = (int)(dl - z); 
     while (w > 0) {
-        t->DwordBuf[ x ] = t->Padding[ x ] =
-            (t->Padding[ x ] & ~(0x000000ff << (24 - y)))
-                | ((dp[ z ] & 0x000000ff) << (24 - y));
+        t->DwordBuf[x] = t->Padding[x] =
+            (t->Padding[x] & ~(0x000000ff << (24 - y)))
+                | ((dp[z] & 0x000000ff) << (24 - y));
         y = y + 8; x += (y >> 5); y &= 0x1f; z ++;
         w --;
     }
 
     /** Append 0x80 */
-    t->Padding[ x ] |= 0x00000080 << (24 - y); 
+    t->Padding[x] |= 0x00000080 << (24 - y); 
 
     /** Save Old Value */
     for (w = 0; w < 8; w ++)
     {
-        t->reg[ w ] = reg[ w ];
+        t->reg[w] = reg[w];
     }
     
     /** Check if Length + 0x80 could append */
     t->DwordBufBytes =  x * 4  + (y >> 3);
     if (t->DwordBufBytes > 55) {
         sha256_calc_block(t->Padding, reg);
-        for (w = 0; w < 16; w ++) { t->Padding[ w ] = 0; }
+        for (w = 0; w < 16; w ++) { t->Padding[w] = 0; }
     }
         
     /** Append Length */
-    t->Padding[ 15 ] = t->ByteNumLo << 3;
-    t->Padding[ 14 ] = ((t->ByteNumLo >> 29) & 0x3) | (t->ByteNumHi << 3);
+    t->Padding[15] = t->ByteNumLo << 3;
+    t->Padding[14] = ((t->ByteNumLo >> 29) & 0x3) | (t->ByteNumHi << 3);
     sha256_calc_block(t->Padding, reg);
 
     /** Output ordered Value -- big endian order */
     for (w = 0, x = 0; w < 32; w ++, x = (x + 8) & 0x1f) {
         /** high byte first output value */
-        t->Value[ w ] = (char)(reg[ w >> 2 ] >> (24 - x));
+        t->Value[w] = (char)(reg[w >> 2] >> (24 - x));
     }
 
     return 0;
@@ -168,19 +168,19 @@ static uint64_t sha256_calc_block(int* dp, int* rp)
     /** extend 16s dword to 64 dwords */    
     for (x = 16; x < 64; x ++) {
 
-        t0 =  (((dp[ x - 15 ] >> 7) & 0x1ffffff)  | (dp[ x - 15 ] << 25)) ^ 
-              (((dp[ x - 15 ] >> 18) & 0x3fff)    | (dp[ x - 15 ] << 14)) ^ 
-              ((dp[ x - 15 ] >> 3 ) & 0x1fffffff);
+        t0 =  (((dp[x - 15] >> 7) & 0x1ffffff)  | (dp[x - 15] << 25)) ^ 
+              (((dp[x - 15] >> 18) & 0x3fff)    | (dp[x - 15] << 14)) ^ 
+              ((dp[x - 15] >> 3 ) & 0x1fffffff);
 
-        t1 =  (((dp[ x - 2 ] >> 17) & 0x7fff) | (dp[ x - 2 ] << 15)) ^ 
-              (((dp[ x - 2 ] >> 19) & 0x1fff) | (dp[ x - 2 ] << 13)) ^ 
-              ((dp[ x - 2 ] >> 10) & 0x3fffff);
+        t1 =  (((dp[x - 2] >> 17) & 0x7fff) | (dp[x - 2] << 15)) ^ 
+              (((dp[x - 2] >> 19) & 0x1fff) | (dp[x - 2] << 13)) ^ 
+              ((dp[x - 2] >> 10) & 0x3fffff);
 
-        dp[ x ] = dp[ x - 16 ] + t0 + dp[ x - 7 ] + t1;
+        dp[x] = dp[x - 16] + t0 + dp[x - 7] + t1;
     }
 
     /** init value */
-     a = rp[0]; b = rp[1]; c = rp[2]; d = rp[3]; e = rp[4]; f = rp[5]; g = rp[6]; h = rp[ 7 ];
+     a = rp[0]; b = rp[1]; c = rp[2]; d = rp[3]; e = rp[4]; f = rp[5]; g = rp[6]; h = rp[7];
 
     /** main loop */
     for (x = 0; x < 64; x ++) {
@@ -195,7 +195,7 @@ static uint64_t sha256_calc_block(int* dp, int* rp)
              (((e >> 11) & 0x1fffff) | (e << 21)) ^ 
              (((e >> 25) & 0x7f) | (e << 7));
         t0 = (e & f) ^ ((~e) & g);
-        t1 = h + t1 + t0 + sha256_key[ x ] + dp[ x ];
+        t1 = h + t1 + t0 + sha256_key[x] + dp[x];
         h=g; g=f; f=e; e=d+t1; d=c; c=b; b=a; a=t1+t2; 
     }
 
