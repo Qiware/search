546788d0c98c6a8b2d191afd65afb9d2f40aa93a
Qifeng.zou <Qifeng.zou.job@gmail.com>
2015-06-30 21:06:57 +0800

    [Listend]: 解决程序运行几个小时后出现COREDUMP的问题 - 由于进行三目运算MIN()时, 使用了多进程/多线程共享的变量，而三目运算MIN()是非原子的, 从而导致比较操作后而返回操作前共享内存值发生变化, 从而导致后续处理出现内存溢出, 程序COREDUMP - 此问题非常难以发现原因，费了我九牛二虎之力!

--- src/exec/listend/lsnd_dist.c
+++ src/exec/listend/lsnd_dist.c
@@ -100,8 +100,10 @@ void *lsnd_dsvr_routine(void *_ctx)
  **     dsvr: 分发服务
  **输出参数: NONE
  **返    回: 0:成功 !0:失败
- **实现描述:
- **注意事项: 消息结构: 流水信息 + 消息头 + 消息体
+ **实现描述: 消息结构: 流水信息 + 消息头 + 消息体
+ **注意事项: 千万勿将共享变量参与MIN()三目运算, 否则可能出现严重错误!!!!且很难找出原因!
+ **          原因: MIN()不是原子运算, 使用共享变量可能导致判断成立后, 而返回时共
+ **                享变量的值可能被其他进程或线程修改, 导致出现严重错误!
  **作    者: # Qifeng.zou # 2015.06.20 #
  ******************************************************************************/
 static int lsnd_dsvr_cmd_dist_hdl(lsnd_cntx_t *ctx, lsnd_dsvr_t *dsvr)
@@ -112,8 +114,9 @@ static int lsnd_dsvr_cmd_dist_hdl(lsnd_cntx_t *ctx, lsnd_dsvr_t *dsvr)
     void *addr[LSND_DIST_POP_NUM];
 
 LSND_AGAIN_MPOP:
-    /* > 获取弹出个数 */
-    num = MIN(shm_queue_used(ctx->sendq), LSND_DIST_POP_NUM);
+    /* > 获取弹出个数(注意: 勿将共享变量参与MIN()三目运算, 否则可能出现严重错误!!!)  */
+    num = shm_queue_used(ctx->sendq); /* 注意: 参加运算前将变量放在局部变量中 */
+    num = MIN(num, LSND_DIST_POP_NUM);
     if (0 == num)
     {
         return LSND_OK;
@@ -121,7 +124,7 @@ LSND_AGAIN_MPOP:
 
     /* > 弹出发送数据 */
     num = shm_queue_mpop(ctx->sendq, addr, num);
-    if (0== num)
+    if (0 == num)
     {
         goto LSND_AGAIN_MPOP;
     }
