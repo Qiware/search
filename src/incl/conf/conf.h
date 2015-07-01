1823fa7c7113e5bbaa955a995adb7aa528ea3eef
Qifeng.zou <Qifeng.zou.job@gmail.com>
2015-07-01 22:40:33 +0800

    [Conf]: 添加配置头文件

---
+++ src/incl/conf/conf.h
@@ -0,0 +1,14 @@
+/******************************************************************************
+ ** Coypright(C) 2014-2024 Xundao technology Co., Ltd
+ **
+ ** 文件名: conf.h
+ ** 版本号: 1.0
+ ** 描  述: 公用配置定义
+ ** 作  者: # Qifeng.zou # Wed 01 Jul 2015 10:07:30 PM CST #
+ ******************************************************************************/
+#if !defined(__CONF_H__)
+#define __CONF_H__
+
+#define LSND_DSVR_CMD_PATH  "../temp/listend/dsvr.usck"
+
+#endif /*__CONF_H__*/
