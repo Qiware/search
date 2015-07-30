7e08ddde993afbe25f5eec3b6e644a0bbc841020
Qifeng.zou <Qifeng.zou.job@gmail.com>
2015-07-30 23:37:08 +0800

    [TOOLS]: 添加启动Oprofile的脚本

---
+++ tools/script/oprofile.sh
@@ -0,0 +1,18 @@
+################################################################################
+## Coypright(C) 2014-2024 Xundao technology Co., Ltd
+##
+## 文件名: oprofile.sh
+## 版本号: 1.0
+## 描  述: 
+## 作  者: # Qifeng.zou # Wed 29 Jul 2015 12:02:39 PM CST #
+################################################################################
+#!/bin/bash
+
+opcontrol --shutdown
+opcontrol --deinit
+echo 0 > /proc/sys/kernel/nmi_watchdog
+
+opcontrol --init
+opcontrol --no-vmlinux
+opcontrol --reset
+opcontrol --start
