9a725efc936512154692f2d15649b81a13fe6c31
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2015-09-24 16:50:54 +0800

    [REDIS]: 将各类类型数据操作分离

---
+++ src/incl/redis/redis.h
@@ -0,0 +1,32 @@
+#if !defined(__REDIS_H__)
+#define __REDIS_H__
+
+#include "comm.h"
+#include "list.h"
+#include <hiredis/hiredis.h>
+
+#include "redis_hash.h"
+#include "redis_list.h"
+
+#define REDIS_CONN_TMOUT_SEC    (30)    /* 连接超时:秒 */
+#define REDIS_CONN_TMOUT_USEC   (0)     /* 连接超时:微妙 */
+
+/* Redis配置 */
+typedef struct
+{
+    char ip[IP_ADDR_MAX_LEN];           /* IP */
+    int port;                           /* 端口号 */
+} redis_conf_t;
+
+/* Redis集群对象 */
+typedef struct
+{
+    int num;                            /* REDIS对象数 */
+#define REDIS_MASTER_IDX (0)            /* MASTER索引 */
+    redisContext **redis;               /* REDIS对象(注: [0]为Master [1~num-1]为Slave */
+} redis_clst_t;
+
+redis_clst_t *redis_clst_init(const redis_conf_t *conf, int num);
+void redis_clst_destroy(redis_clst_t *clst);
+
+#endif /*__REDIS_H__*/
--- src/incl/redis/redis_list.h
+++ src/incl/redis/redis_list.h
@@ -1,33 +1,9 @@
-#if !defined(__REDIS_H__)
-#define __REDIS_H__
+#if !defined(__REDIS_LIST_H__)
+#define __REDIS_LIST_H__
 
 #include "comm.h"
 #include "list.h"
 #include <hiredis/hiredis.h>
-
-#define REDIS_CONN_TMOUT_SEC    (30)    /* 连接超时:秒 */
-#define REDIS_CONN_TMOUT_USEC   (0)     /* 连接超时:微妙 */
-
-/* Redis配置 */
-typedef struct
-{
-    char ip[IP_ADDR_MAX_LEN];       /* IP */
-    int port;                       /* 端口号 */
-} redis_conf_t;
-
-/* Redis集群对象 */
-typedef struct
-{
-    int num;                        /* REDIS对象数 */
-#define REDIS_MASTER_IDX (0)        /* MASTER索引 */
-    redisContext **redis;           /* REDIS对象(注: [0]为Master [1~num-1]为Slave */
-} redis_clst_t;
-
-redis_clst_t *redis_clst_init(const redis_conf_t *conf, int num);
-void redis_clst_destroy(redis_clst_t *clst);
-
-bool redis_hsetnx(redisContext *ctx, const char *hname, const char *key, const char *value);
-int redis_hlen(redisContext *ctx, const char *hname);
 
 /******************************************************************************
  **函数名称: redis_lpop
@@ -97,4 +73,4 @@ int redis_rpushx(redisContext *ctx, const char *ln, const char *value);
  ******************************************************************************/
 #define redis_rpop_lpush(ctx, sln, dln) redisCommand(ctx, "RPOPLPUSH %s %s", sln, dln)
 
-#endif /*__REDIS_H__*/
+#endif /*__REDIS_LIST_H__*/
