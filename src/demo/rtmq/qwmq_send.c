5a7bb114431fccd1f93100887387b21dff2ee367
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2016-01-01 22:39:09 +0800

    [DEMO]: 添加QW队列的测试代码

---
+++ src/demo/rtmq/qwmq_send.c
@@ -0,0 +1,152 @@
+#include <signal.h>
+#include <sys/time.h>
+
+#include "mesg.h"
+#include "syscall.h"
+#include "qwmq_sd_send.h"
+#include "qwmq_sd_ssvr.h"
+
+#define __RTMQ_DEBUG_SEND__
+
+/******************************************************************************
+ **函数名称: qwmq_send_debug 
+ **功    能: 发送端调试
+ **输入参数: 
+ **     ctx: 上下文信息
+ **输出参数: NONE
+ **返    回: 0:Success !0:Failed
+ **实现描述: 
+ **注意事项: 
+ **作    者: # Qifeng.zou # 2014.03.26 #
+ ******************************************************************************/
+#define LOOP        (100000)
+#define USLEEP      (10)
+#define SIZE        (4096)
+
+int qwmq_send_debug(qwsd_cntx_t *ctx, int secs)
+{
+    size_t idx = 0;
+    double sleep2 = 0;
+    struct timeval stime, etime;
+    int total = 0, fails = 0;
+    char data[SIZE];
+    mesg_search_word_req_t *req;
+
+    for (;;)
+    {
+        gettimeofday(&stime, NULL);
+        sleep2 = 0;
+        fails = 0;
+        total = 0;
+        for (idx=0; idx<LOOP; idx++)
+        {
+            req = (mesg_search_word_req_t *)data;
+
+            snprintf(req->words, sizeof(req->words), "%s", "BAIDU");
+
+            if (qwsd_cli_send(ctx, MSG_SEARCH_WORD_REQ, req, sizeof(mesg_search_word_req_t)))
+            {
+                idx--;
+                usleep(2);
+                sleep2 += USLEEP*1000000;
+                ++fails;
+                continue;
+            }
+
+            total++;
+        }
+
+        gettimeofday(&etime, NULL);
+        if (etime.tv_usec < stime.tv_usec)
+        {
+            etime.tv_sec--;
+            etime.tv_usec += 1000000;
+        }
+
+        fprintf(stderr, "%s() %s:%d\n"
+                "\tstime:%ld.%06ld etime:%ld.%06ld spend:%ld.%06ld\n"
+                "\tTotal:%d fails:%d\n",
+                __func__, __FILE__, __LINE__,
+                stime.tv_sec, stime.tv_usec,
+                etime.tv_sec, etime.tv_usec,
+                etime.tv_sec - stime.tv_sec,
+                etime.tv_usec - stime.tv_usec,
+                total, fails);
+
+    }
+
+    pause();
+
+    return 0;
+}
+
+static void qwmq_setup_conf(qwsd_conf_t *conf, int port)
+{
+    conf->nodeid = 1;
+
+    snprintf(conf->auth.usr, sizeof(conf->auth.usr), "qifeng");
+    snprintf(conf->auth.passwd, sizeof(conf->auth.passwd), "111111");
+
+    snprintf(conf->ipaddr, sizeof(conf->ipaddr), "127.0.0.1");
+
+    conf->port = port;
+    conf->send_thd_num = 1;
+    conf->send_buff_size = 5 * MB;
+    conf->recv_buff_size = 2 * MB;
+
+    conf->sendq.max = 2048;
+    conf->sendq.size = 4096;
+}
+
+int main(int argc, const char *argv[])
+{
+    int port;
+    log_cycle_t *log;
+    qwsd_cntx_t *ctx;
+    qwsd_conf_t conf;
+
+    if (2 != argc)
+    {
+        fprintf(stderr, "Didn't special port!");
+        return -1;
+    }
+
+    memset(&conf, 0, sizeof(conf));
+
+    signal(SIGPIPE, SIG_IGN);
+
+    nice(-20);
+
+    port = atoi(argv[1]);
+    qwmq_setup_conf(&conf, port);
+
+    log = log_init(LOG_LEVEL_DEBUG, "./qwmq_ssvr.log", "../temp/log.key");
+    if (NULL == log)
+    {
+        fprintf(stderr, "errmsg:[%d] %s!", errno, strerror(errno));
+        return -1;
+    }
+
+    ctx = qwsd_init(&conf, log);
+    if (NULL == ctx) 
+    {
+        fprintf(stderr, "Initialize send-server failed!");
+        return -1;
+    }
+
+    if (qwsd_launch(ctx))
+    {
+        fprintf(stderr, "Start up send-server failed!");
+        return -1;
+    }
+
+#if defined(__RTMQ_DEBUG_SEND__)
+    qwmq_send_debug(ctx, 5);
+#endif /*__RTMQ_DEBUG_SEND__*/
+
+    while (1) { pause(); }
+
+    fprintf(stderr, "Exit send server!");
+
+    return 0;
+}
