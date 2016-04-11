82a6bc01d3ca66f866d4a1e85b76b747c45e7a81
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2016-04-11 20:56:26 +0800

    [MESG]: 统一进行消息ID定义

--- src/incl/mesg.h
+++ src/incl/mesg.h
@@ -14,39 +14,39 @@
 /* 消息类型 */
 typedef enum
 {
-    MSG_TYPE_UNKNOWN                        /* 未知消息 */
+    MSG_TYPE_UNKNOWN                    /* 未知消息 */
 
-    , MSG_SEARCH_WORD_REQ                   /* 搜索关键字-请求 */
-    , MSG_SEARCH_WORD_RSP                   /* 搜索关键字-应答 */
+    , MSG_SEARCH_WORD_REQ               /* 搜索关键字-请求 */
+    , MSG_SEARCH_WORD_RSP               /* 搜索关键字-应答 */
 
-    , MSG_INSERT_WORD_REQ                   /* 插入关键字-请求 */
-    , MSG_INSERT_WORD_RSP                   /* 插入关键字-应答 */
+    , MSG_INSERT_WORD_REQ               /* 插入关键字-请求 */
+    , MSG_INSERT_WORD_RSP               /* 插入关键字-应答 */
 
-    , MSG_PRINT_INVT_TAB_REQ                /* 打印倒排表-请求 */
-    , MSG_PRINT_INVT_TAB_RSP                /* 打印倒排表-应答 */
+    , MSG_PRINT_INVT_TAB_REQ            /* 打印倒排表-请求 */
+    , MSG_PRINT_INVT_TAB_RSP            /* 打印倒排表-应答 */
 
-    , MSG_QUERY_CONF_REQ                    /* 查询配置信息-请求 */
-    , MSG_QUERY_CONF_RSP                    /* 反馈配置信息-应答 */
+    , MSG_QUERY_CONF_REQ                /* 查询配置信息-请求 */
+    , MSG_QUERY_CONF_RSP                /* 反馈配置信息-应答 */
 
-    , MSG_QUERY_WORKER_STAT_REQ             /* 查询工作信息-请求 */
-    , MSG_QUERY_WORKER_STAT_RSP             /* 反馈工作信息-应答 */
+    , MSG_QUERY_WORKER_STAT_REQ         /* 查询工作信息-请求 */
+    , MSG_QUERY_WORKER_STAT_RSP         /* 反馈工作信息-应答 */
 
-    , MSG_QUERY_WORKQ_STAT_REQ              /* 查询工作队列信息-请求 */
-    , MSG_QUERY_WORKQ_STAT_RSP              /* 反馈工作队列信息-应答 */
+    , MSG_QUERY_WORKQ_STAT_REQ          /* 查询工作队列信息-请求 */
+    , MSG_QUERY_WORKQ_STAT_RSP          /* 反馈工作队列信息-应答 */
 
-    , MSG_SWITCH_SCHED_REQ                  /* 切换调度-请求 */
-    , MSG_SWITCH_SCHED_RSP                  /* 反馈切换调度信息-应答 */
+    , MSG_SWITCH_SCHED_REQ              /* 切换调度-请求 */
+    , MSG_SWITCH_SCHED_RSP              /* 反馈切换调度信息-应答 */
 
-    , MSG_SUB_REQ                           /* 订阅-请求 */
-    , MSG_SUB_ACK                           /* 订阅-应答 */
+    , MSG_SUB_REQ                       /* 订阅-请求 */
+    , MSG_SUB_RSP                       /* 订阅-应答 */
 
-    , MSG_TYPE_TOTAL                        /* 消息类型总数 */
+    , MSG_TYPE_TOTAL                    /* 消息类型总数 */
 } mesg_type_e;
 
 /* 通用协议头 */
 typedef struct
 {
-    uint32_t type;                      /* 消息类型 Range: mesg_type_e */
+    mesg_type_e type;                   /* 消息类型 Range: mesg_type_e */
 #define MSG_FLAG_SYS   (0)              /* 0: 系统数据类型 */
 #define MSG_FLAG_USR   (1)              /* 1: 自定义数据类型 */
     uint32_t flag;                      /* 标识量(0:系统数据类型 1:自定义数据类型) */
@@ -87,19 +87,21 @@ typedef struct
 
 #define mesg_total_len(body_len) (sizeof(mesg_header_t) + body_len);
 
+////////////////////////////////////////////////////////////////////////////////
 /* 搜索消息结构 */
 #define SRCH_WORD_LEN       (128)
 typedef struct
 {
-    char words[SRCH_WORD_LEN];              /* 搜索关键字 */
+    char words[SRCH_WORD_LEN];          /* 搜索关键字 */
 } mesg_search_word_req_t;
 
+////////////////////////////////////////////////////////////////////////////////
 /* 插入关键字-请求 */
 typedef struct
 {
-    char word[SRCH_WORD_LEN];               /* 关键字 */
-    char url[URL_MAX_LEN];                  /* 关键字对应的URL */
-    int freq;                               /* 频率 */
+    char word[SRCH_WORD_LEN];           /* 关键字 */
+    char url[URL_MAX_LEN];              /* 关键字对应的URL */
+    int freq;                           /* 频率 */
 } mesg_insert_word_req_t;
 
 /* 插入关键字-应答 */
@@ -107,8 +109,8 @@ typedef struct
 {
 #define MESG_INSERT_WORD_FAIL   (0)
 #define MESG_INSERT_WORD_SUCC   (1)
-    int code;                               /* 应答码 */
-    char word[SRCH_WORD_LEN];               /* 关键字 */
+    int code;                           /* 应答码 */
+    char word[SRCH_WORD_LEN];           /* 关键字 */
 } mesg_insert_word_rsp_t;
 
 #define mesg_insert_word_resp_hton(rsp) do { \
@@ -117,6 +119,41 @@ typedef struct
 
 #define mesg_insert_word_resp_ntoh(rsp) do { \
     (rsp)->code = ntohl((rsp)->code); \
+} while(0)
+
+////////////////////////////////////////////////////////////////////////////////
+/* 订阅-请求 */
+typedef struct
+{
+    mesg_type_e type;                   /* 订阅类型(消息类型) */
+    int nodeid;                         /* 结点ID */
+} mesg_sub_req_t;
+
+#define mesg_sub_req_hton(req) do { /* 主机 -> 网络 */\
+    (req)->type = htonl((req)->type); \
+    (req)->nodeid = htonl((req)->nodeid); \
+} while(0)
+
+#define mesg_sub_req_ntoh(req) do { /* 网络 -> 主机 */\
+    (req)->type = ntohl((req)->type); \
+    (req)->nodeid = ntohl((req)->nodeid); \
+} while(0)
+
+/* 订阅-应答 */
+typedef struct
+{
+    int code;                           /* 应答码 */
+    mesg_type_e type;                   /* 订阅类型(消息类型) */
+} mesg_sub_rsp_t;
+
+#define mesg_sub_rsp_hton(rsp) do { /* 主机 -> 网络 */\
+    (rsp)->code = htonl((rsp)->code); \
+    (rsp)->type = htonl((rsp)->type); \
+} while(0)
+
+#define mesg_sub_rsp_ntoh(rsp) do { /* 网络 -> 主机 */\
+    (rsp)->code = ntohl((rsp)->code); \
+    (rsp)->type = ntohl((rsp)->type); \
 } while(0)
 
 #endif /*__MESG_H__*/
