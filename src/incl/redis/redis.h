93483169b1a30bf82f24860377eac1183e980fcb
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2016-04-26 00:51:39 +0800

    [Redis]: 删除redis集群的管理 -- 可使用codis集群替代

--- src/incl/redis/redis.h
+++ src/incl/redis/redis.h
@@ -8,9 +8,6 @@
 #include "redis_hash.h"
 #include "redis_list.h"
 
-#define REDIS_CONN_TMOUT_SEC    (30)    /* 连接超时:秒 */
-#define REDIS_CONN_TMOUT_USEC   (0)     /* 连接超时:微妙 */
-
 /* Redis配置 */
 typedef struct
 {
@@ -19,15 +16,7 @@ typedef struct
     char passwd[PASSWD_MAX_LEN];        /* 密码 */
 } redis_conf_t;
 
-/* Redis集群对象 */
-typedef struct
-{
-    int num;                            /* REDIS对象数 */
-#define REDIS_MASTER_IDX (0)            /* MASTER索引 */
-    redisContext **redis;               /* REDIS对象(注: [0]为Master [1~num-1]为Slave */
-} redis_clst_t;
-
-redis_clst_t *redis_clst_init(const redis_conf_t *conf, int num);
-void redis_clst_destroy(redis_clst_t *clst);
+redisContext *redis_init(const redis_conf_t *conf, int sec);
+#define redis_destroy(redis) redisFree(redis)
 
 #endif /*__REDIS_H__*/
--- src/lib/redis/redis.c
+++ src/lib/redis/redis.c
@@ -4,78 +4,22 @@
 #include "redis.h"
 
 /******************************************************************************
- **函数名称: redis_clst_init
- **功    能: 初始化Redis集群
+ **函数名称: redis_init
+ **功    能: 初始化Redis
  **输入参数: 
  **     conf: Redis配置数组
- **     num: 数组长度
  **输出参数:
- **返    回: Redis上下文
+ **返    回: Redis对象
  **实现描述: 
- **     1. 申请内存空间
- **     2. 连接Master
- **     3. 依次连接Slave
  **注意事项: 
- **     conf[0]: 表示的是Master配置
- **     conf[1~num-1]: 表示的是副本配置
  **作    者: # Qifeng.zou # 2014.11.04 #
  ******************************************************************************/
-redis_clst_t *redis_clst_init(const redis_conf_t *conf, int num)
+redisContext *redis_init(const redis_conf_t *conf, int sec)
 {
-    int idx;
     struct timeval tv;
-    redis_clst_t *clst;
 
-    /* 1. 申请内存空间 */
-    clst = (redis_clst_t *)calloc(1, sizeof(redis_clst_t));
-    if (NULL == clst) {
-        return NULL;
-    }
+    tv.tv_sec = sec;
+    tv.tv_usec = 0;
 
-    clst->redis = (redisContext **)calloc(num, sizeof(redisContext *));
-    if (NULL == clst->redis) {
-        free(clst);
-        return NULL;
-    }
-
-    /* 2. 依次连接REDIS */
-    clst->num = num;
-    for (idx=0; idx<num; ++idx) {
-        tv.tv_sec = REDIS_CONN_TMOUT_SEC;
-        tv.tv_usec = REDIS_CONN_TMOUT_USEC;
-
-        clst->redis[idx] = redisConnectWithTimeout(conf[idx].ip, conf[idx].port, tv);
-        if (clst->redis[idx]->err) {
-            redis_clst_destroy(clst);
-            return NULL;
-        }
-    }
-
-    return clst;
-}
-
-/******************************************************************************
- **函数名称: redis_clst_destroy
- **功    能: 销毁Redis集群
- **输入参数: 
- **     clst: Redis集群
- **输出参数: NONE
- **返    回: VOID
- **实现描述: 依次销毁集群各成员上下文
- **注意事项: 
- **作    者: # Qifeng.zou # 2014.11.04 #
- ******************************************************************************/
-void redis_clst_destroy(redis_clst_t *clst)
-{
-    int idx;
-
-    for (idx=0; idx<clst->num; ++idx) {
-        if (NULL != clst->redis[idx]) {
-            redisFree(clst->redis[idx]);
-            clst->redis[idx] = NULL;
-        }
-    }
-
-    free(clst->redis);
-    free(clst);
+    return redisConnectWithTimeout(conf->ip, conf->port, tv);
 }
