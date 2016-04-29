1c848c92010bdbf533530d4d12d511bc57d0b870
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2016-04-30 00:32:40 +0800

    [SEARCH]: 添加一个倒排结点

--- bin/search.sh
+++ bin/search.sh
@@ -8,9 +8,9 @@ ulimit -c unlimited
 export LD_LIBRARY_PATH='/usr/lib/x86_64-linux-gnu:../lib'
 
 # 启动服务
-./frwder -l debug -c ../conf/frwder.xml -d     # 转发服务
-./invertd -l debug -c ../conf/invertd.xml -d   # 倒排服务
-./invertd -l debug -c ../conf/invertd-30002.xml -d   # 倒排服务
-./listend -l debug -c ../conf/listend.xml -d   # 代理服务
+./frwder -l error -c ../conf/frwder.xml -d     # 转发服务
+./invertd -l error -c ../conf/invertd.xml -d   # 倒排服务
+./invertd -l error -c ../conf/invertd-30002.xml -d   # 倒排服务
+./listend -l error -c ../conf/listend.xml -d   # 代理服务
 
 ./watch.sh      # 监控进程状态
