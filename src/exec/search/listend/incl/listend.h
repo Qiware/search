443ad997f5e227d639d89a86fb9ce9095cc7bacd
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2016-05-29 13:22:40 +0800

    [LISTEND]: 删除无效代码

--- src/exec/search/listend/incl/listend.h
+++ src/exec/search/listend/incl/listend.h
@@ -25,20 +25,11 @@ typedef struct
     char *conf_path;                        /* 配置路径 */
 } lsnd_opt_t;
 
-/* 分发对象 */
-typedef struct
-{
-    int cmd_sck_id;                         /* 命令套接字 */
-    fd_set rdset;                           /* 可读集合 */
-} lsnd_dsvr_t;
-
 /* 全局对象 */
 typedef struct
 {
     lsnd_conf_t conf;                       /* 配置信息 */
     log_cycle_t *log;                       /* 日志对象 */
-
-    lsnd_dsvr_t dsvr;                       /* 分发服务 */
 
     agent_cntx_t *agent;                    /* 代理服务 */
     rtmq_proxy_t *frwder;                   /* FRWDER服务 */
@@ -46,8 +37,5 @@ typedef struct
 
 int lsnd_getopt(int argc, char **argv, lsnd_opt_t *opt);
 int lsnd_usage(const char *exec);
-
-int lsnd_dsvr_init(lsnd_cntx_t *ctx);
-void *lsnd_dsvr_routine(void *_ctx);
 
 #endif /*__LISTEND_H__*/
