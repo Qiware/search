a810394062d965538aa2779dcd7472b5feda09ff
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2016-09-05 15:43:53 +0800

    [DEMO]: 完善红黑树结点

--- src/demo/rbtree/rbt_demo.c
+++ src/demo/rbtree/rbt_demo.c
@@ -9,6 +9,7 @@
 #include "comm.h"
 #include "rb_tree.h"
 
+#define SID_BEG     (100000)
 #define SID_NUM     (10)
 
 typedef struct
@@ -23,7 +24,12 @@ void rbt_trav_print_hdl(rbt_data_t *data, void *args)
     fprintf(stderr, "idx:%u id:%lu\n", ++idx, data->id);
 }
 
-static rtb_data_cmp_cb(const rbt_data_t *data1, const rbt_data_t *data2)
+void rbt_print_cb(rbt_data_t *data)
+{
+    fprintf(stderr, "id:%lu", data->id);
+}
+
+static int rtb_data_cmp_cb(const rbt_data_t *data1, const rbt_data_t *data2)
 {
     return (data1->id - data2->id);
 }
@@ -40,13 +46,13 @@ int main(void)
     }
 
     for (n=0; n<2; n++) {
-        for (idx=0; idx<SID_NUM; ++idx) {
+        for (idx=SID_BEG; idx<SID_BEG+SID_NUM; ++idx) {
             data = (rbt_data_t *)calloc(1, sizeof(rbt_data_t));
             data->id = idx;
             rbt_insert(rbt, (void *)data);
         }
 
-        for (idx=0; idx<SID_NUM; ++idx) {
+        for (idx=SID_BEG; idx<SID_BEG+SID_NUM; ++idx) {
             //rbt_delete(rbt, (void *)&idx, sizeof(idx), (void **)&data);
             //free(data);
             key.id = idx;
@@ -56,7 +62,7 @@ int main(void)
     }
 
     rbt_trav(rbt, (trav_cb_t)rbt_trav_print_hdl, NULL);
-    rbt_print(rbt);
+    rbt_print(rbt, (print_cb_t)rbt_print_cb);
 
     key.id = 3;
     data = rbt_query(rbt, &key);
