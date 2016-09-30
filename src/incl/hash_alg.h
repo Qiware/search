a49d9f2e667365f67039c689aaee8db66ee2bb5e
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2016-09-30 22:32:54 +0800

    [COMM]: 将基础组建分离出去, 放各程序共享组建

--- src/incl/hash_alg.h
+++
@@ -1,9 +0,0 @@
-#if !defined(__HASH_ALG_H__)
-#define __HASH_ALG_H__
-
-#include "comm.h"
-
-uint64_t hash_time33(const char *str);
-uint64_t hash_time33_ex(const void *addr, size_t len);
-
-#endif /*__HASH_ALG_H__*/
