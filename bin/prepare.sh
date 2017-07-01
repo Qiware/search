587dc9cb32cd53c0081fcfa009fabbe8a74ea7de
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2017-07-01 22:00:55 +0800

    [SHELL]: 完善预处理脚本

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH='/usr/lib/x86_64-linux-gnu:../lib:/usr/lib/:/usr/local/lib/'   # 动态链接库路径
+export LD_LIBRARY_PATH='/usr/lib/x86_64-linux-gnu:../lib:/usr/lib/:/usr/local/lib/:../../cctrl/lib'   # 动态链接库路径
 
 # 准备阶段
 #sudo redis-server /etc/redis/redis_slave_6380.conf
